1c84749168bfea4d68ad1224b68b1be0fc624f8b
Minji <77886530+Skaze97@users.noreply.github.com>
2021-05-31 14:02:51 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,17 @@
 self.addEventListener("install" , e => {
     e.waitUntil(
         caches.open("static").then(cache =>{
-          return cache.addAll(["./","./style.css","./images/icon192.png","./images/icon512.png","./images/maskable_icon192.png", ]); 
+          return cache.addAll(["./","./style.css","./images/icon192.png","./images/icon512.png","./images/maskable_icon192.png","./rfid.js","./versuch.js","./versuchStyle.css" ]); 
         })
     );
 });
 
 self.addEventListener("fetch", e =>{
-    console.log("Intercepting fetch request for  : $(e.request.url)'");
-})
+    e.respondWith(
+        caches.matches(e.request).then(response =>{
+            return response || fetch(e.request);
+        })
+    );
+});
 
 

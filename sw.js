dd2faf690335f6de046ef87366cbb834328ffa71
Minji <77886530+Skaze97@users.noreply.github.com>
2021-05-31 13:47:12 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,13 @@
 self.addEventListener("install" , e => {
-    console.log("Install!");
+    e.waitUntil(
+        caches.open("static").then(cache =>{
+          return cache.addAll(["./","./style.css","./images/icon192.png","./images/icon512.png","./images/maskable_icon192.png", ]); 
+        })
+    );
+});
+
+self.addEventListener("fetch", e =>{
+    console.log("Intercepting fetch request for  : $(e.request.url)'");
 })
+
 

7ae187d90cf0d78e1ee9a704415de24e2617da47
Minji <77886530+Skaze97@users.noreply.github.com>
2021-05-31 19:17:35 +0200

    Delete Unused

--- Website/server.js
+++
@@ -1,8 +0,0 @@
-
-//based on node.js express framework (uncomplete version since this pwa has to run on https,could not be implemented yet)
-var express = require('express');
-var app = express();
-app.use(express.static('public'));
-app.listen(8080, () =>{
-    console.log('Your application is on http://localhost:8080');
-});

a1853a6d1437c4c1bc05e94b220efd2c99835d15
Minji <77886530+Skaze97@users.noreply.github.com>
2021-05-31 11:57:15 +0200

    Test
    Test

--- sw.js
+++ sw.js
@@ -1,17 +1,4 @@
-const filesToCache = [
- '/',
- 'versuchStyle.css',
- 'index.html',
- 'versuch.js',
-]
-
-const staticCacheName = 'TestCache' ;
-self.addEventListener('install', event => {
-    console.log('Trying to install server Worker and cache files');
-    event.waitUntil(
-        cache.open(staticCacheName).then(cache =>{
-            return cache.addAll(filesToCache);
-        })
-    );
-});
+self.addEventListener("install" , e => {
+    console.log("Install!");
+})
 
